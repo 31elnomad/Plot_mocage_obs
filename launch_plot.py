2b7fcac5f694114937fd2f26604295d8cc6e81e9
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-20 15:50:35 +0100

    Update launch_plot.py

--- launch_plot.py
+++ launch_plot.py
@@ -1,2 +1,36 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on the Wed Nov 20
+
+@author baclesm
+"""
+import argparse
+
+def create_argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-cfg', '--cfg',
+                        help='config_file',
+                        metavar='cfg')
+    parser.add_argument('-start', '--start',
+                         nargs='?', const=None,
+                         help='date du début',
+                         metavar='ddeb')
+    parser.add_argument('-dfin', '--dfin',
+                        nargs='?', const=None,
+                        help='date de fin',
+                        metavar='dfin')      
+    parser.add_argument('-deltat', '--deltat',
+                        nargs='?', const=None,
+                        help='pas de temps',
+                        metavar='deltat')
+    parser.add_argument('-listdate', '--listdate',
+                        nargs='?', const=None,
+                        help='list de date',
+                        metavar='listdate')
+    config_opts = parser.parse_args()
+    return config_opts
+    
 if __ name__ == "__main__":
-    print('ok)
+    config_opts = create_argparse()
+    
