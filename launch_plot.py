6aea96a7fcefaf329bbffe79cf1a2816321c028e
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 14:38:24 +0100

    Update launch_plot.py

--- launch_plot.py
+++ launch_plot.py
@@ -78,6 +78,7 @@ if __name__ == "__main__":
         sys.exit(1)
     if config_class.config["global"]["type_plot"] == 'map':
         # Initialise la classe PlotMap
+        plot_class = PlotMap(config_class)
         try:
             plot_class = PlotMap(config_class)
         except Exception as e:
