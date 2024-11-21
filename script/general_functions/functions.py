8e0b68df3edb45382e83dec15118aac7def0342c
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 16:12:48 +0100

    Update functions.py

--- script/general_functions/functions.py
+++ script/general_functions/functions.py
@@ -18,28 +18,34 @@ def list_of_plot(order, listvar, listexp, listdate, nligne, ncol):
         if order[0] == 'var':
             plotz = len(listvar)
             if order[1] == 'exp':
-              plotx = cmp_nplot(listexp, ncol)
-              ploty = cmp_nplot(listdate, nligne)
+                plotx = cmp_nplot(listexp, ncol)
+                ploty = cmp_nplot(listdate, nligne)
+                case = 1
             elif order[1] == 'date':
-              plotx = cmp_nplot(listdate, ncol)
-              ploty = cmp_nplot(listexp, nligne)   
+                plotx = cmp_nplot(listdate, ncol)
+                ploty = cmp_nplot(listexp, nligne)  
+                case = 2
         elif order[0] == 'exp':
             plotz = len(listexp)
             if order[1] == 'var':
                 plotx = cmp_nplot(listvar, ncol)
                 ploty = cmp_nplot(listdate, nligne)
+                case = 3
             elif order[1] == 'date':
                 plotx = cmp_nplot(listdate, ncol)
-                ploty = cmp_nplot(listvar, nligne)  
+                ploty = cmp_nplot(listvar, nligne) 
+                case = 4
         elif order[0] == 'date':
             plotz = len(listdate)
             if order[1] == 'var':
                 plotx = cmp_nplot(listvar, ncol)
                 ploty = cmp_nplot(listexp, nligne)
+                case = 5
             elif order[1] == 'exp':
                 plotx = cmp_nplot(listexp, ncol)
                 ploty = cmp_nplot(listvar, nligne)  
-        return [plotz, plotx, ploty]
+                case = 6
+        return [plotz, plotx, ploty], case
     
     def cmp_nplot(list, number):
         """
@@ -51,13 +57,43 @@ def list_of_plot(order, listvar, listexp, listdate, nligne, ncol):
           n = n + 1
         return n
 
-    def param_per_plot(list_dim, order, listvar, listexp, listdate, nligne, ncol):
+    def param_per_plot(list_dim, order, listvar, listexp, listdate, nligne, ncol, case):
         param_plot_dict = {}
-        for z, x, y in zip(list(np.arange(list_dim[0])), list(np.arange(list_dim[1])), list(np.arange(list_dim[2]))):
-            print(z, x, y)
+        for x in range(list_dim[1]):
+            for y in range(list_dim[2]):
+                for z in range(list_dim[0]):
+                    param_plot_dict[str((x+1)*(y+1)*(z+1))] = listdict_param(case,
+                                                                             listvar,
+                                                                             listexp,
+                                                                             listdate,
+                                                                             nligne,
+                                                                             ncol,
+                                                                             x,
+                                                                             y,
+                                                                             z)
+
+    def listdict_param(case, listvar, listexp, listdate, nligne, ncol, x, y, z):
+        if case == 1:
+            var1 = 'var'
+            list1 = listvar[z]
+            var2 = 'exp'
+            var3 = 'date'
+            if len(listexp[ncol*x:]) <= ncol:
+                list2 = listexp[ncol*x:]
+            else:
+                list2 = listexp[ncol*x:ncol*(x+1)]
+            if len(listdate[nligne*y:]) <= nligne:
+                list3 = listexp[nligne*y:]
+            else:
+                list3 = listexp[nligne*y:nligne*(y+1)]
+        output = [{var1: list1},
+                  {var2: list2},
+                  {var3: list3}]
+        print(output)
+        return output
         
-    list_dim = number_plot(order, listvar, listexp, listdate, nligne, ncol)  
-    param_per_plot(list_dim, order, listvar, listexp, listdate, nligne, ncol)
+    list_dim, case = number_plot(order, listvar, listexp, listdate, nligne, ncol)  
+    param_per_plot(list_dim, order, listvar, listexp, listdate, nligne, ncol, case)
     
     return list_dim
 
