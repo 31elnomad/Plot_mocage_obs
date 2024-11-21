a06097ab7cf5ca79c42f55daefd4491e956d62f6
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 15:10:00 +0100

    Create functions.py

---
+++ script/general_functions/functions.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21
+
+@author: baclesm
+"""
+import collections
+
+def list_of_plot(order, listvar, listexp, listdate, nligne, ncol):
+
+    def number_plot(order, listvar, listexp, listdate, nligne, ncol):
+        print("map, order: {}".format(order))
+        test = collections.Counter(['var', 'exp', 'date']) == collections.Counter(order)
+        if len(order) != 3 or test is False:
+            raise Exception("Problème avec la variable order dans la section map. Doit contenir 'var', 'exp' et 'date'.")
+        if order[0] == 'var':
+            plotz = len(listvar)
+            if order[1] == 'exp':
+              plotx = cmp_nplot(listexp, ncol)
+              ploty = cmp_nplot(listdate, nligne)
+            elif order[1] == 'date':
+              plotx = cmp_nplot(listdate, ncol)
+              ploty = cmp_nplot(listexp, nligne)   
+        elif order[0] == 'exp':
+            plotz = len(listexp)
+            if order[1] == 'var':
+                plotx = cmp_nplot(listvar, ncol)
+                ploty = cmp_nplot(listdate, nligne)
+            elif order[1] == 'date':
+                plotx = cmp_nplot(listdate, ncol)
+                ploty = cmp_nplot(listvar, nligne)  
+        elif order[0] == 'date':
+            plotz = len(listdate)
+            if order[1] == 'var':
+                plotx = cmp_nplot(listvar, ncol)
+                ploty = cmp_nplot(listexp, nligne)
+            elif order[1] == 'exp':
+                plotx = cmp_nplot(listexp, ncol)
+                ploty = cmp_nplot(listvar, nligne)  
+      return [plotz, plotx, ploty]
+    
+    def cmp_nplot(list, number):
+        """
+        list = listvar, listdate or lisexp
+        number = self.ncol ou self.nligne
+        """
+        n = len(list) // number
+        if len(list) % number > 0:
+          n = n + 1
+        return n
+
+
+  
