fdc7027ee083df0d56cd2bb2529ea39df11d362c
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 07:37:38 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21
 
+@author: baclesm
+"""
+
+import os
+import sys
+sys.path.append(os.path.abspath("../"))
+from read_config import Config
+
+class Netcdf(Config):
+
+  def __init__(self):
+    Config.__init__(self, config_opts)
