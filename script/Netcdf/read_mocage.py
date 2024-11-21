576f94a49a6019e97f9016918203403a9445d114
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 12:37:12 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -10,11 +10,11 @@ import os
 import sys
 
 class Netcdf_mocage:
+  
   def __init__(self, config_class):
-    self.plot = {}
-    for plot in config_class.config["global"]["plot_list"].split(","):
-      if plot == "map":
-        self.config_map = config_class.config[plot]
-        self.case == int(config_class.config
-      print(config_class.config[plot]["listexp"])
-      
+    self.nligne = int(config_class.config['global']['nligne'])
+    self.ncol = int(config_class.config['global']['ncol'])
+    if config_class.config['global']['type_plot'] == "map":
+      self.config_plot = config_class.config['map']
+    elif config_class.config['global']['type_plot'] == 'cut':
+      self.config_plot = config_class.config['cut']
