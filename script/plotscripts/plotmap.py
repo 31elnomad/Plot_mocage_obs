4580990f816e6e0da42001ca975329f06b92db5b
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 15:11:15 +0100

    Update plotmap.py

--- script/plotscripts/plotmap.py
+++ script/plotscripts/plotmap.py
@@ -5,7 +5,8 @@ Created on Mon Nov 21
 
 @author: baclesm
 """
-import collections
+
+from functions import list_of_plot
 
 class PlotMap:
     
@@ -17,28 +18,15 @@ class PlotMap:
         self.listexp = self.config_plot['listexp'].split(',')
         self.order = self.config_plot['order'].split(',')
 
-        self.number_plot()
+        list_of_plot(self.order,
+                     self.listvar,
+                     self.listexp,
+                     self.listdate,
+                     self.nligne,
+                     self.ncol
         
-        """# Initialisation d'un indicateur pour savoir si une expérience est trouvée
-        is_experiment_found = any(
-            "exp" in config_class.config[plot]['listexp']
-            for plot in self.plot_list
-        )
-        if is_experiment_found:
-            # Import conditionnel
-            from read_mocage import Netcdf_mocage
-            self.nc_class = Netcdf_mocage(config_class)
-        else:
-            self.nc_class = None"""
 
-    def number_plot(self):
-        print("map, order: {}".format(self.order))
-        test = collections.Counter(['var', 'exp', 'date']) == collections.Counter(self.order)
-        if len(self.order) != 3 or test is False:
-            raise Exception("Problème avec la variable order dans la section map. Doit contenir 'var', 'exp' et 'date'.")
-        for element in self.order:
-            idx = self.order.index(element)
-            print(element, idx)
+            
        
             
             
