c9f5a8d8c2a2ae5ba4b19849dac7b5ce505aa973
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 12:02:51 +0100

    Update plotmap.py

--- script/plotscripts/plotmap.py
+++ script/plotscripts/plotmap.py
@@ -8,7 +8,14 @@ Created on Mon Nov 21
 class Plot:
     
     def __init__(self, config_class):
-        # Initialisation d'un indicateur pour savoir si une expérience est trouvée
+        self.nligne = int(config_class.config['global']['nligne'])
+        self.ncol = int(config_class.config['global']['ncol'])
+        self.config_plot = config_class.config['map']
+        self.listvar = self.config_plot['listvar'].split(',')
+        self.listexp = self.config_plot['listexp'].split(',')
+        self.case = int(self.config_plot['case'])
+        
+        """# Initialisation d'un indicateur pour savoir si une expérience est trouvée
         is_experiment_found = any(
             "exp" in config_class.config[plot]['listexp']
             for plot in self.plot_list
@@ -18,4 +25,28 @@ class Plot:
             from read_mocage import Netcdf_mocage
             self.nc_class = Netcdf_mocage(config_class)
         else:
-            self.nc_class = None
+            self.nc_class = None"""self.plot_list = config_class.config["global"]["plot_list"].split(",")
+
+    def number_plot(self):
+        print("map, case {}".format(self.case))
+        if self.case == 1:
+            nplot_x = len(self.listexp) // self.ncol + 1
+            nplot_y = len(self.listdate) // self.nligne + 1
+            nplot = len(self.listvar) * nplot_x * nplot_y
+        elif self.case == 2:
+            nplot_x = len(self.listexp) // self.ncol + 1
+            nplot_y = len(self.listvar) // self.nligne + 1
+            nplot = len(self.listdate) * nplot_x * nplot_y
+        elif self.case == 3:
+            nplot_x = len(self.listvar) // self.ncol + 1
+            nplot_y = len(self.listdate) // self.nligne + 1
+            nplot = len(self.listexp) * nplot_x * nplot_y
+        else:
+            raise Exception("Case doit valoir 1, 2 ou 3 pour les cartes")
+
+    def param_to_plot(self):
+        
+            
+    
+        
+        
