cfe620c33fe6723b5e3ba403398db14435e7cf8b
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-21 14:33:13 +0100

    Update plotmap.py

--- script/plotscripts/plotmap.py
+++ script/plotscripts/plotmap.py
@@ -5,6 +5,8 @@ Created on Mon Nov 21
 
 @author: baclesm
 """
+import collections
+
 class Plot:
     
     def __init__(self, config_class):
@@ -13,7 +15,7 @@ class Plot:
         self.config_plot = config_class.config['map']
         self.listvar = self.config_plot['listvar'].split(',')
         self.listexp = self.config_plot['listexp'].split(',')
-        self.case = int(self.config_plot['case'])
+        self.order = self.config_plot['order'].split(',')
         
         """# Initialisation d'un indicateur pour savoir si une expérience est trouvée
         is_experiment_found = any(
@@ -25,26 +27,15 @@ class Plot:
             from read_mocage import Netcdf_mocage
             self.nc_class = Netcdf_mocage(config_class)
         else:
-            self.nc_class = None"""self.plot_list = config_class.config["global"]["plot_list"].split(",")
+            self.nc_class = None"""self.plot_list = config_class.config["global"]["plot_list"].split(",")"""
 
     def number_plot(self):
-        print("map, case {}".format(self.case))
-        if self.case == 1:
-            nplot_x = len(self.listexp) // self.ncol + 1
-            nplot_y = len(self.listdate) // self.nligne + 1
-            nplot = len(self.listvar) * nplot_x * nplot_y
-        elif self.case == 2:
-            nplot_x = len(self.listexp) // self.ncol + 1
-            nplot_y = len(self.listvar) // self.nligne + 1
-            nplot = len(self.listdate) * nplot_x * nplot_y
-        elif self.case == 3:
-            nplot_x = len(self.listvar) // self.ncol + 1
-            nplot_y = len(self.listdate) // self.nligne + 1
-            nplot = len(self.listexp) * nplot_x * nplot_y
-        else:
-            raise Exception("Case doit valoir 1, 2 ou 3 pour les cartes")
-
-    def param_to_plot(self):
+        print("map, order: {}".format(self.order))
+        list_to_test = ['var', 'exp', 'date']
+        print(collections.Counter(list_to_test) == collections.Counter(self.order))
+       
+            
+            
         
             
     
