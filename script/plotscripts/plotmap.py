f6170b469afb3929a150dce535fe2a3c35676ecb
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-22 15:21:27 +0100

    Update plotmap.py

--- script/plotscripts/plotmap.py
+++ script/plotscripts/plotmap.py
@@ -89,7 +89,7 @@ class PlotMap:
                 if len(self.param_one_plot) == 5:
                     list_param_plot[3] = self.param_one_plot[3][keys4][0]
                 self.param[self.ncol*j + i] = list_param_plot
-            print(slef.param)
+            print(self.param)
             quit()
 
     def __main_plotmap__(self, param_one_plot):
