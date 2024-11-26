736ec672e311980533e21619517435cf8c17ad25
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-26 09:26:04 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -90,7 +90,7 @@ class Netcdf_mocage:
             from get_data import get_mocage
             out_file = get_mocage(exp=self.nameexp,
                                   vconf=self.conf,
-                                  date,self.date,
+                                  date=self.date,
                                   domain=self.domain.lower(),
                                   term=self.echeance,
                                   cutoff=self.suffix,
