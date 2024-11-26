af40f06c719c6705661dcd45bdbed6dee6901e02
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-26 16:30:49 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -213,7 +213,7 @@ class Netcdf_mocage:
         if self.config_nc['getfile'].lower() in ['t', 'true']:
             ds = xr.open_dataset(os.path.join(self.dirtmp, self.outfile_name))
         self.nblev = ds.attrs['levels']
-        print(ds.coord['lon'])
+        print(ds.coords['lon'])
         #ds = ds[self.var[0]].squeeze()
         #print(ds)
        
