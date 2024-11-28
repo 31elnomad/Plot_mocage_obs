b0e3746666a8f6626edd79f04bd6a97c0e2987f1
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 15:25:40 +0100

    Update plot2d.py

--- script/general_functions/plot2d.py
+++ script/general_functions/plot2d.py
@@ -14,12 +14,14 @@ def __print_colorbar__(fig, sc, config_plot):
     print('ok1')
     colorbar = config_plot['colorbar'].split(':')
     if colorbar[0].lower() not in ['t', 'true']:
+        print('ok1_2')
         if colorbar[1].lower() in ['v']:
             fig.subplots_adjust(right=0.8)
             cbar_ax = fig.add_axes([0.84,0.12,0.02, 0.7])
             orientation = 'vertical'
         elif colorbar[1].lower() in ['h']:
-            Fig.subplots_adjust(bottom=0.19)
+            print('ok1_3')
+            fig.subplots_adjust(bottom=0.19)
             cbar_ax = fig.add_axes([0.15, 0.12, 0.7, 0.02])
             orientation = 'horizontal'
         cbar = fig.colorbar(sc, 
