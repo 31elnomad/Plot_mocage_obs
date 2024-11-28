118b29b94226e59f8acd77f03949c4050fb04865
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 14:04:38 +0100

    Update contourf.py

--- script/general_functions/contourf.py
+++ script/general_functions/contourf.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def __contourf__(ax, nc_obj, nlevs, vmin, vmax, **kwargs):
-        try:
-            if not 'cmap' in kwargs:
-                kwargs['cmap'] = 'jet'
-        except:
+    try:
+        if not 'cmap' in kwargs:
             kwargs['cmap'] = 'jet'
-        levels = np.linspace(vmin, vmax, nlevs)
-        ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, levels, kwargs=kwargs)
-        return ax
+    except:
+        kwargs['cmap'] = 'jet'
+    levels = np.linspace(vmin, vmax, nlevs)
+    sc = ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, levels, kwargs=kwargs)
+    return ax, sc
