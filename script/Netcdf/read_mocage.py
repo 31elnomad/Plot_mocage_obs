3dd98938b52469d45cb5d8d1bef6134483390e88
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 16:08:44 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -123,6 +123,7 @@ class Netcdf_mocage:
                             pres_var.append('b_hybr_coord')
                         if 'air_pressure_at_surface' not in pres_var:
                             pres_var.append('air_pressure_at_surface')
+                        os.remove(os.path.join(self.dirtmp, self.outfile_name))
                         ds = self.__get_file__(False, HOST, pres_var)
                     else:
                         print('File {} already contains {}'.format(self.outfile_name, self.var[0]))
