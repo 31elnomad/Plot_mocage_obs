03488f1bd2cfc0c4264f7cbe8c1c77461671a8df
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 10:50:04 +0100

    Update plotmap.py

--- script/plotscripts/plotmap.py
+++ script/plotscripts/plotmap.py
@@ -151,6 +151,8 @@ class PlotMap:
         self.create_list_param()
         with Pool(5) as p:
             results = p.map(self.plot_para, self.param)
+        plt.savefig('test.png')
+        plt.close()
         
 
             
