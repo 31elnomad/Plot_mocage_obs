7b049a063a52208aee0558b12cfda9103a3874c2
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 14:54:20 +0100

    Update and rename contourf.py to plot2d.py

---
+++ script/general_functions/plot2d.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def __contourf__(ax, nc_obj, pas, vmin, vmax, **kwargs):
+    try:
+        if not 'cmap' in kwargs:
+            kwargs['cmap'] = 'jet'
+    except:
+        kwargs['cmap'] = 'jet'
+    levels = np.arange(vmin, vmax+pas, pas)
+    sc = ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, levels, kwargs=kwargs)
+    return ax, sc
+
+def print_colorbar(fig, sc, nc_obj, config_plot):
+    colorbar = config_plot['colorbar'].split(':')
+    if colorbar[0].lower() not in ['t', 'true']:
+        if colorbar[1].lower() in ['v']:
+            fig.subplots_adjust(right=0.8)
+            cbar_ax = fig.add_axes([0.84,0.12,0.02, 0.7])
+            orientation = 'vertical'
+        elif colorbar[1].lower() in ['h']:
+            Fig.subplots_adjust(bottom=0.19)
+            cbar_ax = Fig.add_axes([0.15, 0.12, 0.7, 0.02])
+            orientation = 'horizontal'
+        cbar = fig.colorbar(sc, 
+                        cax=cbar_ax,
+                        orientation=orientation,
+                        extend=nc_mocage.extend
+                        )
