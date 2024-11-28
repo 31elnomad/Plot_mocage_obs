cc7c0d2d05751e6eef79fd6a17bafb6f49ecbf67
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 13:03:51 +0100

    Update plotmap.py

--- script/plotscripts/plotmap.py
+++ script/plotscripts/plotmap.py
@@ -174,7 +174,7 @@ class PlotMap:
         self.param_one_plot = param_one_plot
         self.create_fig()
         self.create_list_param()
-        with Pool(1) as p:
+        with Pool(5) as p:
             results = p.map(self.plot_para, self.param)
         from concat_plot import __concat_plot__
         __concat_plot__(self.fig, self.axs, results, 'a')
