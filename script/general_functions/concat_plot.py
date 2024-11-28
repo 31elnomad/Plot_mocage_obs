684f693ae8c85821fd470d0afc1ad40463de7d97
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 15:06:38 +0100

    Update concat_plot.py

--- script/general_functions/concat_plot.py
+++ script/general_functions/concat_plot.py
@@ -8,7 +8,5 @@ def __concat_plot__(fig, axs, filenames, finale_name_plot):
         img = plt.imread(filename)
         ax.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree())
         ax.axis('off')
-    plt.tight_layout()
-    plt.savefig("test.png")
-    plt.close()
+    return axs
     
