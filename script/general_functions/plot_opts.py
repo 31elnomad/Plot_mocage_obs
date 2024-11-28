8ced1e665f2a17c1e6936f044259b81df5015039
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 15:43:02 +0100

    Create plot_opts.py

---
+++ script/general_functions/plot_opts.py
@@ -0,0 +1,15 @@
+def process_res(results):
+    sc = None
+    obj_data = None
+    var = None
+    filenames = []
+    for r in results:
+        filenames.append(r[0])
+        if sc is None and r[1] is not None:
+            sc = r[1]
+        if obj_data is None and r[2] is not None:
+            obj_data = r[2]
+        if var is not None and r[3] is not None:
+            var = r[3]
+            units = r[4]
+      return filenames, sc, obj_data, ver, units
