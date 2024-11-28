c0ace42aa620a6a60addd0adaa28520cd3518a36
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 13:31:53 +0100

    Update contourf.py

--- script/general_functions/contourf.py
+++ script/general_functions/contourf.py
@@ -1,7 +1,9 @@
 def __contourf__(ax, nc_obj, **kwargs):
-        print(nc_obj.data)
-        print('ok')
-        cmap = self.config_plot['cmap']
+        try:
+            if not 'cmap' in kwargs:
+                kwargs['cmap'] = 'jet'
+        except:
+            kwargs['cmap'] = 'jet'
         nlevs = int(self.config_plot['plot_opt'].split(':')[1])
-        ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, nlevs, cmap=cmap, **kwargs)
+        ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, nlevs, cmap=cmap, kwargs=kwargs)
         return ax
