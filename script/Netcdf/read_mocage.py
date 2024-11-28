d2200bf11a048263485ae386f5b0e97b720c02b1
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 16:02:48 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -123,7 +123,7 @@ class Netcdf_mocage:
                             pres_var.append('b_hybr_coord')
                         if 'air_pressure_at_surface' not in pres_var:
                             pres_var.append('air_pressure_at_surface')
-                        print('ok')
+                        print(pres_var)
                         quit()
                         ds = self.__get_file__(False, HOST, pres_var)
                     else:
