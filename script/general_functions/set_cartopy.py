d6f544aeac4f303ad987ae067873e6a47442a78c
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 11:47:23 +0100

    Update set_cartopy.py

--- script/general_functions/set_cartopy.py
+++ script/general_functions/set_cartopy.py
@@ -79,7 +79,9 @@ def _set_cartopy_(obj_plot, nc_obj, ax, ligne, col):
                    nc_obj.latbnd[0],
                    nc_obj.latbnd[1]],
                   crs=mapproj)
-    plt.show()
+    plt.savefig('test.png')
+    plt.close()
+    quit()
     return ax
         
           
