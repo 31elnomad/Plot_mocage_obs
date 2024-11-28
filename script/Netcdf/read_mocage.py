933874a7e58806e0325b5bc97f8b4f7cd2aff363
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 13:57:54 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -375,7 +375,7 @@ class Netcdf_mocage:
 
     def mask_data(self):
         self.extend = 'both'
-        if float(self.config_plot['vmin']:
+        if float(self.config_plot['vmin']):
             self.extend = 'max'
         if self.config_plot['maskmin'].lower() in ['true', 't'] and self.config_plot['maskmax'].lower() in ['true', 't']:
             self.extend = None
