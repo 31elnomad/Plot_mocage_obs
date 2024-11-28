a2179d88601f5e91bbeeadf46dad88b117bf656a
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 13:35:41 +0100

    Update contourf.py

--- script/general_functions/contourf.py
+++ script/general_functions/contourf.py
@@ -4,5 +4,5 @@ def __contourf__(ax, nc_obj, nlevs, **kwargs):
                 kwargs['cmap'] = 'jet'
         except:
             kwargs['cmap'] = 'jet'
-        ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, nlevs, cmap=cmap, kwargs=kwargs)
+        ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, nlevs, kwargs=kwargs)
         return ax
