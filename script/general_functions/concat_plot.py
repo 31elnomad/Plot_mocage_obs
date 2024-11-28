938d74ecdbb6d7b93a43bd2a46270649943edefc
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-28 13:00:38 +0100

    Create concat_plot.py

---
+++ script/general_functions/concat_plot.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+def __concat__plot(fig, axs, filenames, finale_name_plot):
+    for ax, filename in zip(axes.flat, filenames):
+        img = plt.imread(filename)
+        ax.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree())
+        ax.axis('off')
+    plt.tight_layout()
+    plt.savefig("test.png")
+    plt.close()
+    
