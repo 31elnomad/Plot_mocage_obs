168a913254c8fecd475e98a1fb13ef79af9d79fb
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-29 11:15:10 +0100

    Update read_mocage_hdat.py

--- script/observations/read_mocage_hdat.py
+++ script/observations/read_mocage_hdat.py
@@ -11,6 +11,7 @@ class obs_mocage:
                                       int(date[4:6]),
                                       int(date[6:8]),
                                       int(date[8:10]))
+        self.config_plot = config[config['type_plot']]
         self.var = var
         self.kwargs = kwargs
 
@@ -25,10 +26,75 @@ class obs_mocage:
         self.listfile = glob.glob(dirfile)
 
     def read_h5(self):
+        # Initialize data and coordinates arrays
+        self.lon = []
+        self.lat = []
+        self.data = []
+        d2 = self.date
+        d1 = d2 - timedelta(hours=1)
+        TimeBnd = (d1, d2)
+        self.crete_listfile()
         for file in self.listfile:
             if self.pseudo.lower() in ['iasi_a', 'iasi_b', 'iasi_c']:
                 obj = daimonobs.DefInstrument(self.config_h5['instrname'],
                                               'IasiInstr',
-                                              species=kwargs['species'])
-          
+                                              species=self.kwargs['species'])
+            elif self.pseudo.lower() in ['iasi_a_lh', 'iasi_b_lh', 'iasi_c_lh']:
+                obj = daimonobs.DefInstrument(self.config_h5['instrname'],
+                                              'IasiInstr',
+                                              species=self.kwargs['species'],
+                                              nmaxlevs=self.kwargs['nmaxlevs'])
+            elif self.pseudo.lower() in ['omi']:
+                obj = daimonobs.DefInstrument(self.config_h5['instrname'],
+                                              'OMIInstr',
+                                              species=self.kwargs['species'])
+            elif self.pseudo.lower() in ['tropomi']:
+                obj = daimonobs.DefInstrument(self.config_h5['instrname'],
+                                              'TropomiInstr',
+                                              species=self.kwargs['species'])
+            elif self.pseudo.lower() in ['tropomi_lh']:
+                obj = daimonobs.DefInstrument(self.config_h5['instrname'],
+                                              'TropomiConcInstr',
+                                              species=self.kwargs['species'],
+                                              nmaxlevs=self.kwargs['nmaxlevs'])
+            elif self.pseudo.lower() in ['viirs']:
+                Obj = daimonobs.DefInstrument(self.config_h5['instrname'],
+                                              'ViirsAOD',
+                                              wv=self.kwargs['wv'])
+            elif self.pseudo.lower() in ['modis']:
+                Obj = daimonobs.DefInstrument(self.config_h5['instrname'],
+                                              'MODIS_AOD',
+                                              wv=self.kwargs['wv'])
+
+            # Read data from the current file
+            if self.config_h5['type'].lower() in ['h5_sim', 'HSTAT']:
+                Obj.Read(file, ReadAssim={'Region': self.config_h5['domain'], 'ObsType': 'Y'})
+            else:
+                Obj.Read(file)
+            # Select data within the specified time bounds
+            Obj.Select(TimeBnd=TimeBnd, LonBnd=None, LatBnd=None)
+
+            # Append Lon, Lat, and Data to their respective lists
+            self.lon.extend(Obj.lons)
+            self.lat.extend(Obj.lats)
+            if self.pseudo.lower() in ['iasi_a_lh', 'iasi_b_lh', 'iasi_c_lh', 'tropomi_lh']:
+                self.data.extend(Obj.col[self.kwargs['species']])
+            elif self.pseudo.lower() in ['iasi_a', 'iasi_b', 'iasi_c', 'tropomi']:
+                self.data.extend(Obj.pcol[self.kwargs['species']])
+            elif self.pseudo.lower()  in ['modis', 'viirs']:
+                self.data.extend(Obj.aod[str(self.kwargs['wv'])])
+        # Convert Lon, Lat, and Data lists to NumPy arrays
+        Lon = np.array(Lon)
+        Lat = np.array(Lat)
+        Data = np.array(Data)
+        if self.config_plot['maskmin'].lower() in ['true', 't']:
+            if instr[:5] not in ['MODIS', 'VIIRS']:
+                if self.pseudo.lower() in ['iasi_a_lh', 'iasi_b_lh', 'iasi_c_lh', 'tropomi_lh']:
+                    if len(self.data.shape) > 1:
+                        raise Exception("Le cas avec plusieurs colonnes n'a pas été codé pour les observations")
+            mk = self.data >= float(self.config_plot['vmin'])
+            self.lon = self.lon[mk]
+            self.at = self.lat[mk]
+            self.data = self.data[mk]
+            
 
