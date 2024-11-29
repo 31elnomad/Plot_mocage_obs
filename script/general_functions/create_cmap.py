762c6bf0098e3b72d20b97a77f90dbf18c605e04
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-29 10:01:19 +0100

    Create create_cmap.py

---
+++ script/general_functions/create_cmap.py
@@ -0,0 +1,73 @@
+import matplotlib
+
+def__create_cmap__(cmap):
+    """
+    Create a custom colormap for visualization.
+
+    Returns:
+        matplotlib.colors.LinearSegmentedColormap: A custom colormap.
+
+    This function generates a custom colormap using a predefined set of colors and
+    creates a LinearSegmentedColormap object for use in data visualization.
+    """
+    # Define color palettes for different color orders
+    if cmap == 'wbrb':
+        colors = ['white', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red', 'magenta', 'brown']
+    elif cmap == 'brbw':
+        colors = ['brown', 'magenta', 'red', 'orange', 'yellow', 'green', 'blue', 'cyan', 'white']
+    elif cmap == 'brb':
+        colors = ['cyan', 'blue', 'green', 'yellow', 'orange', 'red', 'magenta']
+    elif cmap == 'bwr':
+        colors = ['blue', 'cyan', 'white', 'yellow', 'red']
+    elif cmap == 'pwb':
+        colors = ['purple', 'blue', 'cyan', 'white', 'yellow', 'red', 'brown']
+    color_under = colors[0]
+    color_over = colors[-1]
+    color_bad = '0.80'  # light gray
+
+    # Number of colors
+    nslot = len(colors)
+
+    # Get red/green/blue arrays for color extensions
+    red_under, green_under, blue_under = matplotlib.colors.colorConverter.to_rgb(color_under)
+    red_over, green_over, blue_over = matplotlib.colors.colorConverter.to_rgb(color_over)
+
+    # Initialize color dictionary
+    cdict = {'red': [], 'green': [], 'blue': []}
+
+    # Intersection values in [0, 1]
+    slots = np.arange(nslot) / float(nslot - 1)
+
+    # Loop over colors
+    for islot in range(nslot):
+        # Convert to RGB value
+        red, green, blue = matplotlib.colors.colorConverter.to_rgb(colors[islot])
+
+        # Current intersection
+        x = slots[islot]
+
+        # Fill colors
+        if islot == 0:
+            red_y0, red_y1 = red_under, red
+            green_y0, green_y1 = green_under, green
+            blue_y0, blue_y1 = blue_under, blue
+        elif islot == nslot - 1:
+            red_y0, red_y1 = red, red_over
+            green_y0, green_y1 = green, green_over
+            blue_y0, blue_y1 = blue, blue_over
+        else:
+            red_y0, red_y1 = red, red
+            green_y0, green_y1 = green, green
+            blue_y0, blue_y1 = blue, blue
+
+        # Add tuples to the color dictionary
+        cdict['red'].append((x, red_y0, red_y1))
+        cdict['green'].append((x, green_y0, green_y1))
+        cdict['blue'].append((x, blue_y0, blue_y1))
+
+    # Create the colormap
+    cmap = matplotlib.colors.LinearSegmentedColormap('mymap', cdict, N=256)
+    cmap.set_under(color_under)
+    cmap.set_over(color_over)
+    cmap.set_bad(color_bad, alpha=1)
+    return cmap
