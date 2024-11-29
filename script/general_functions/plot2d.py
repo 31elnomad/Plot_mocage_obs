ffade9582851079c347aad60fc1dc280fc9f7dd3
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-29 09:39:15 +0100

    Update plot2d.py

--- script/general_functions/plot2d.py
+++ script/general_functions/plot2d.py
@@ -1,14 +1,37 @@
 import numpy as np
 
 def __contourf__(ax, nc_obj, pas, vmin, vmax, **kwargs):
-    try:
-        if not 'cmap' in kwargs:
-            kwargs['cmap'] = 'jet'
-    except:
+    if not 'cmap' in kwargs:
         kwargs['cmap'] = 'jet'
     levels = np.arange(vmin, vmax+pas, pas)
     sc = ax.contourf(nc_obj.lon, nc_obj.lat, nc_obj.data, levels, kwargs=kwargs)
     return ax, sc
+    
+def __scatter__(ax, nc_obj, markersize, vmin, vmax, **kwargs):
+    if not 'cmap' in kwargs:
+        kwargs['cmap'] = 'jet'
+    if len(nc_obj.data.shape) == 2:
+        mapx, mapy = __gridmap__(nc_obj)
+        sc = ax.scatter(
+            mapx,
+            mapy,
+            s=float(markersize),
+            c=nc_obj.data,
+            marker='s',
+            zorder=10,
+            vmin=vmin,
+            vmax=vmax,
+            alpha=1.,
+            kwarg=kwargs)
+    return ax, sc
+        
+def __gridmap__(nc_obj):
+    if nc_obj.lonbnd[0] != nc_obj.lonbnd[1] and nc_obj.latbnd[0] != nc_obj.latbnd[1]:
+        mapx = np.repeat(nc_obj.lon.reshape(1, -1), nc_obj.lat.shape[0], axis=0)
+        mapy = np.repeat(nc_obj.lat.reshape(-1, 1), nc_obj.lon.shape[0], axis=1)
+    else:
+        raise Exception("__gridmap__ n'est pas codé pour ce cas")
+    return mapx, mapy
 
 def __print_colorbar__(fig, sc, config_plot, obj_data, unit, var):
     colorbar = config_plot['colorbar'].split(':')
