dde578565703f598855c1611e2363475a504cfd1
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-29 09:13:03 +0100

    Update plot_opts.py

--- script/general_functions/plot_opts.py
+++ script/general_functions/plot_opts.py
@@ -46,6 +46,8 @@ def read_csv(filename, name):
     import pandas as pd
     df = pd.read_csv(filename, sep = ';')
     tmp = df[df['name'] == name]
+    if len(tmp) == 0:
+        raise Exception("Le volcan {} n'a pas été trouvé dans le fichier {}".format(name, filename)
     lon = np.array(tmp['longitude'])
     lat = np.array(tmp['latitude'])
     return lon, lat
