d4d3ba98a2b37522df915dc336d5408debb4861d
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-08 15:49:47 +0100

    Update concat_plot.py

--- script/general_functions/concat_plot.py
+++ script/general_functions/concat_plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
 def __concat_plot__(fig, axs, filenames):
-    if len(axs) > 0:
+    if len(filenames) > 0:
         for ax, filename in zip(axs.flat, filenames):
             img = plt.imread(filename)
             ax.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree())
