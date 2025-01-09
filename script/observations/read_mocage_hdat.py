609bb545aa63a40f59e263be39855768ea6caa5d
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-09 14:36:05 +0100

    Update read_mocage_hdat.py

--- script/observations/read_mocage_hdat.py
+++ script/observations/read_mocage_hdat.py
@@ -17,6 +17,9 @@ class obs_mocage:
         self.config_plot = config_class.config[config_class.config['global']['type_plot']]
         self.kwargs = kwargs
         self.delta = float(config_class.config['observations']['delta'])
+        self.central_longitude = 0.
+        if 'central_longitude' in kwargs:
+            self.central_longitude = kwargs['central_longitude']
 
     def create_listfile(self):
         if self.config_h5['type'].lower() in ['h5_sim', 'h5_obs']:
@@ -108,21 +111,18 @@ class obs_mocage:
                 lonbnd[1]= 180.
                 lonbnd = tuple(lonbnd)
                 obj_copy.Select(TimeBnd=TimeBnd, LonBnd=lonbnd, LatBnd=self.latbnd)
-                self.lon.extend(obj_copy.lons)
-                self.lat.extend(obj_copy.lats)
                 self.__read_h5__(obj_copy)
                 lonbnd = list(self.lonbnd)
                 lonbnd[0] = -180.
                 lonbnd = tuple(lonbnd)
                 obj.Select(TimeBnd=TimeBnd, LonBnd=lonbnd, LatBnd=self.latbnd)
             # Append Lon, Lat, and Data to their respective lists
-            self.lon.extend(obj.lons)
-            self.lat.extend(obj.lats)
             self.__read_h5__(obj)
-            
-            
         # Convert Lon, Lat, and Data lists to NumPy arrays
         self.lon = np.array(self.lon)
+        if self.central_longitude == 180.:
+            self.lon[self.lon + self.central_longitude > 180.] -= self.central_longitude
+            self.lon[self.lon + self.central_longitude <= 180.] += self.central_longitude
         self.lat = np.array(self.lat)
         self.data = np.array(self.data)
         if self.config_plot['maskmin'].lower() in ['true', 't']:
@@ -135,6 +135,8 @@ class obs_mocage:
             self.data = self.data[mk]
 
     def __read_h5__(self, obj):
+      self.lon.extend(obj.lons)
+      self.lat.extend(obj.lats)
       if self.pseudo.lower() in ['iasi_a_lh', 'iasi_b_lh', 'iasi_c_lh', 'tropomi_lh']:
           self.data.extend(obj.col[self.kwargs['species']])
       elif self.pseudo.lower() in ['iasi_a', 'iasi_b', 'iasi_c', 'tropomi']:
