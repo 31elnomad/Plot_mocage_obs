4c4f127aebb3b37093b62ea1dabaaf2fe97ddf1e
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-09 14:14:13 +0100

    Update read_mocage_hdat.py

--- script/observations/read_mocage_hdat.py
+++ script/observations/read_mocage_hdat.py
@@ -105,7 +105,7 @@ class obs_mocage:
                 import copy
                 obj_copy = obj
                 lonbnd = self.lonbnd
-                print(lonbnd)
+                print(lonbnd[0])
                 quit()
                 lonbnd[1]= 180.
                 obj_copy.Select(TimeBnd=TimeBnd, LonBnd=lonbnd, LatBnd=self.latbnd)
