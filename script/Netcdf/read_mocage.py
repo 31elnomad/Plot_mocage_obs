36952496d2d87da0bbc5e18dfb61a82993bac902
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-09 11:44:28 +0100

    Update read_mocage.py

--- script/Netcdf/read_mocage.py
+++ script/Netcdf/read_mocage.py
@@ -306,13 +306,13 @@ class Netcdf_mocage:
             idx_lon2 = np.argmin(tmp)
             mask = np.empty_like(self.lon).astype(bool)
             mask[:] = False
-            print('ok1', idx_lon1, idx_lon2)
-            quit()
             if idx_lon1 > idx_lon2:
                 mask[idx_lon1:idx_lon2]  = True
             else:
                 mask[idx_lon1:] = True
                 mask[:idx_lon2] = True
+            print(mask)
+            quit()
             self.lon = self.lon[mask]
             if len(self.data.shape) == 3:
                 self.data = self.data[:, :, mask]
