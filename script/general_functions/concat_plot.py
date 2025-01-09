3f4a0289a4a301014a8351a7b614ee0f9bbbc819
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-09 13:38:54 +0100

    Update concat_plot.py

--- script/general_functions/concat_plot.py
+++ script/general_functions/concat_plot.py
@@ -3,16 +3,20 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
-def __concat_plot__(fig, axs, filenames):
+def __concat_plot__(fig, axs, filenames, type_plot, **kwargs):
     print(filenames)
-    if len(filenames) > 1:
-        for ax, filename in zip(axs.flat, filenames):
-            img = plt.imread(filename)
-            ax.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree())
-            ax.axis('off')
-    else:
-        img = plt.imread(filenames[0])
-        axs.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree())
-        axs.axis('off')
+    central_longitude = 0.
+    if 'central_longitude' in kwargs:
+        central_longitude = kwargs['central_longitude']
+    if type_plot in ['map']:   
+        if len(filenames) > 1:
+            for ax, filename in zip(axs.flat, filenames):
+                img = plt.imread(filename)
+                ax.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree(central_longitude=central_longitude))
+                ax.axis('off')
+        else:
+            img = plt.imread(filenames[0])
+            axs.imshow(img, extent=[-180,180,-90,90], transform=ccrs.PlateCarree(central_longitude=central_longitude))
+            axs.axis('off')
     return axs
     
