a6571197f80e6653f01da0eb5f092996203de38e
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-09 16:07:56 +0100

    Update tropomi.py

--- script/observations/tropomi.py
+++ script/observations/tropomi.py
@@ -1,1 +1,12 @@
-
+def __main_tropomi__(config_class, pseudo, date, lonbnd, latbnd, **kwargs):
+    if config_class[pseudo]['overpass'] == 'T' and config_class[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+        raise Exception ("Not implemented tropomi.py ligne 3")
+    elif config_class[pseudo]['overpass'] == 'T' and config_class[pseudo]['type'] in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+        rom read_mocage_hdat importread_h5
+        raise Exception ("Not implemented tropomi.py ligne 6")
+    elif config_class[pseudo]['overpass'] == 'F' and config_class[pseudo]['type'] in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+        from read_mocage_hdat import read_h5
+        lon, lat, data = read_h5(config_class, pseudo, date, lonbnd, latbnd, kwargs)
+    elif config_class[pseudo]['overpass'] == 'F' and config_class[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+        raise Exception ("Not implemented tropomi.py ligne 9")
+  
