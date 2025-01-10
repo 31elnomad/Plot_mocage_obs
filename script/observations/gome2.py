209196b278e1738d48043667e6b6dcd572c8edaa
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-10 16:01:41 +0100

    Update gome2.py

--- script/observations/gome2.py
+++ script/observations/gome2.py
@@ -99,6 +99,8 @@ def create_masktime(date, time):
     for t in range(len(time)):
         if time[t][0] == njour:
             tmp = time[t][1]/3600000
+            print (int(hour_min), tmp, int(hour_max))
+            quit()
             if tmp > int(hour_min) and tmp <= int(hour_max):
                 kept_time[t] = True
             else:
