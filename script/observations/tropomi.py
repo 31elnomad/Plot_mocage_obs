4096bd8b72d1236969330bc8e27d4b7ab30c277c
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-10 09:38:39 +0100

    Update tropomi.py

--- script/observations/tropomi.py
+++ script/observations/tropomi.py
@@ -1,14 +1,13 @@
 def __main_tropomi__(config_class, pseudo, date, lonbnd, latbnd, **kwargs):
-    print( dict(config_class.items('TROPOMI')))
-    if config_class[pseudo]['overpass'] == 'T' and config_class[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+    if config_class.config[pseudo]['overpass'] == 'T' and config_class.config[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
         raise Exception ("Not implemented tropomi.py ligne 3")
-    elif config_class[pseudo]['overpass'] == 'T' and config_class[pseudo]['type'] in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+    elif config_class.config[pseudo]['overpass'] == 'T' and config_class.config[pseudo]['type'] in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
         from read_mocage_hdat import read_h5
         raise Exception ("Not implemented tropomi.py ligne 6")
-    elif config_class[pseudo]['overpass'] == 'F' and config_class[pseudo]['type'] in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+    elif config_class.config[pseudo]['overpass'] == 'F' and config_class.config[pseudo]['type'] in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
         from read_mocage_hdat import read_h5
         lon, lat, data = read_h5(config_class, pseudo, date, lonbnd, latbnd, kwargs)
-    elif config_class[pseudo]['overpass'] == 'F' and config_class[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
+    elif config_class.config[pseudo]['overpass'] == 'F' and config_class.config[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
         raise Exception ("Not implemented tropomi.py ligne 9")
     return lon, lat, data
   
