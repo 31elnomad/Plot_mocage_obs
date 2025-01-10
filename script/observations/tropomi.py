bf63b976482f9dad1f92232ebf79f62c4113039e
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-10 09:10:05 +0100

    Update tropomi.py

--- script/observations/tropomi.py
+++ script/observations/tropomi.py
@@ -9,4 +9,5 @@ def __main_tropomi__(config_class, pseudo, date, lonbnd, latbnd, **kwargs):
         lon, lat, data = read_h5(config_class, pseudo, date, lonbnd, latbnd, kwargs)
     elif config_class[pseudo]['overpass'] == 'F' and config_class[pseudo]['type'] not in ['HDAT', 'HSTAT', 'h5_sim', 'h5_obs']:
         raise Exception ("Not implemented tropomi.py ligne 9")
+    return lon, lat, data
   
