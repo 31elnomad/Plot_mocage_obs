61e75e28aaed90480c19084c217f398c66c381dc
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-10 13:50:56 +0100

    Update convert_data.py

--- script/general_functions/convert_data.py
+++ script/general_functions/convert_data.py
@@ -5,6 +5,8 @@ def __convert_data__(file_unit, unit, data):
         if unit == 'DU':
             if file_unit == 'molec m-2':
                 data = data / 2.6867e20
+            elif file_unit == 'molec cm-2':
+                data = data / 2.6867e16
         else:
             raise Exception ("Convert {} n'est pas implémenté")
     else:
