71c744910ed809dc96188b259771828bae97ab96
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-10 13:20:11 +0100

    Update gome2.py

--- script/observations/gome2.py
+++ script/observations/gome2.py
@@ -47,10 +47,9 @@ def openfile(file, var):
     elif var[0] in ['CloudType']:
         data = f['CLOUD_PROPERTIES/CloudType'][:]
     elif var[0] in ['SO2', 'SO_2', 'SO_2_tc', 'SO2_tc']:
-        print('ok')
-        data = f['DETAILLED_RESULTS/SO2/VCDCorrected'][:]
-        flag1 = f['DETAILLED_RESULTS/SO2/SO2_Flag'][:]
-        flag = f['DETAILLED_RESULTS/SO2/SO2_Volcano_Flag'][:]
+        data = f['DETAILED_RESULTS/SO2/VCDCorrected'][:]
+        flag1 = f['DETAILED_RESULTS/SO2/SO2_Flag'][:]
+        flag = f['DETAILED_RESULTS/SO2/SO2_Volcano_Flag'][:]
         
             
     print(flag1)
